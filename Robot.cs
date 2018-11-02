ae5b300800cfc89e15e4bb25132f149a5195b629
sohn824 <37131599+sohn824@users.noreply.github.com>
2018-11-02 23:49:44 +0900

    1102
    로봇의 AI 구현중

---
+++ Robot.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    private GameObject player;
+    private Animator robotAnimator;
+    private SpriteRenderer robotSprite;
+    private Rigidbody2D robotRigid;
+    private int robotHP = 30;
+    private bool isNewState = false;
+    private Vector3 velocity = Vector3.zero;
+    private float speed = 1.5f;
+    private float jumpPower = 5.0f;
+    private bool isFind = false;
+    public bool isJumping = false;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            if (player.GetComponent<Player>().currentBullet == Player.CurrentBullet.Default)
+            {
+                robotHP--;
+            }
+            else if (player.GetComponent<Player>().currentBullet == Player.CurrentBullet.RocketLauncher)
+            {
+                robotHP -= 2;
+            }
+            else if (player.GetComponent<Player>().currentBullet == Player.CurrentBullet.FlameShot)
+            {
+                robotHP -= 3;
+            }
+        }
+    }
+    public enum RobotState
+    {
+        Idle,
+        Trace,
+        Jump,
+        Shoot,
+        Die
+    }
+    public RobotState robotState;
+    private void OnEnable()
+    {
+        StartCoroutine("FSMMain");
+    }
+    void Start ()
+    {
+        player = GameObject.Find("Player");
+        robotAnimator = GetComponent<Animator>();
+        robotSprite = GetComponent<SpriteRenderer>();
+        robotRigid = GetComponent<Rigidbody2D>();
+	}	
+	void Update ()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < 30 && isFind == false)
+        {
+            isFind = true;
+            SetState(RobotState.Trace);
+        }
+	}
+    public void SetState(RobotState newState)
+    {
+        isNewState = true;
+        robotState = newState;
+    }
+    IEnumerator FSMMain()
+    {
+        while (true)
+        {
+            isNewState = false;
+            yield return StartCoroutine(robotState.ToString());
+        }
+    }
+    IEnumerator Idle()
+    {
+        do
+        {
+            yield return null;
+            if (isNewState)
+            {
+                break;
+            }
+        } while (!isNewState);
+    }
+    IEnumerator Trace()
+    {
+        do
+        {
+            transform.position += velocity * speed * Time.deltaTime;
+            if (player.transform.position.x < transform.position.x)
+            {
+                velocity = Vector3.left;
+                robotSprite.flipX = true;
+            }
+            else
+            {
+                velocity = Vector3.right;
+                robotSprite.flipX = false;
+            }
+            yield return null;
+        } while (!isNewState);
+    }
+    IEnumerator Jump()
+    {
+        do
+        {
+            if(!isJumping)
+            {
+                robotRigid.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
+                isJumping = true;
+                SetState(RobotState.Trace);
+            }
+            yield return null;
+        } while (!isNewState);
+        isJumping = false;
+    }
+    IEnumerator Shoot()
+    {
+        robotAnimator.SetBool("isShoot", true);
+        do
+        {
+            yield return null;
+        } while (!isNewState);
+        robotAnimator.SetBool("isShoot", false);
+    }
+    IEnumerator Die()
+    {
+        do
+        {
+            yield return null;
+        } while (!isNewState);
+    }
+}
---
+++ RobotJumper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotJumper : MonoBehaviour
+{
+    BoxCollider2D colliderForJump;
+    GameObject player;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ground")
+        {
+            transform.parent.GetComponent<Robot>().SetState(Robot.RobotState.Jump);
+        }
+    }
+    void Start()
+    {
+        colliderForJump = GetComponent<BoxCollider2D>();
+        player = GameObject.Find("Player");
+    }
+    void Update()
+    {
+        if (player.transform.position.x < transform.position.x)
+        {
+            colliderForJump.offset = new Vector2(-0.17f, 0.1f);
+        }
+        else
+        {
+            colliderForJump.offset = new Vector2(0.17f, 0.1f);
+        }
+    }
+}
