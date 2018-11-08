2a4a51b772f58e060d0d39f87ca209907b8111b0
sohn824 <37131599+sohn824@users.noreply.github.com>
2018-11-08 19:02:54 +0900

    1108
    아이템 상자 추가

---
+++ FirstAidKit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstAidKit : MonoBehaviour
+{
+    GameObject player;
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player");
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            player.GetComponent<Player>().playerHP += 3;
+            if(player.GetComponent<Player>().playerHP > 10)
+            {
+                player.GetComponent<Player>().playerHP = 10;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ ItemChest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemChest : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] itemArray;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        int seed = Random.Range(1,4);
+        switch(seed)
+        {
+            case 1:
+                Instantiate(itemArray[0], transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(itemArray[1], transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(itemArray[2], transform.position, Quaternion.identity);
+                break;
+        }
+    }
+}
